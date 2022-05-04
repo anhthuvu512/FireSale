30f70482ea5221b64adb4027da3f77419093dff1
lorraineros <43746757+lorraineros@users.noreply.github.com>
2022-05-04 13:33:07 +0000

    search fixed

--- fire_sale/static/js/items.js
+++ fire_sale/static/js/items.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
-    $('#search-btn').on(types: 'click', selector: function (e) {
+    $('#search-btn').on('click', function (e) {
         e.preventDefault();
         var searchText = $('#search-box').val();
-        $.ajax(url: {
-            url: '/items?search_filter=' + searchText,
+        $.ajax({
+            url: '/sales?search_filter=' + searchText,
             type: 'GET',
             success: function(resp) {
-                var newHtml = resp.data.map(d =>{
-                    return 
+                var newHtml = resp.data.map(d => {
+                    return `<div class="item">
+                                <a href="/sales/${d.id}">
+                                    <img class="item-img" src="${d.firstImage}"/>
+                                    <h4>${d.name}</h4>
+                                </a>
+                            </div>`
                 })
+                $('.items').html(newHtml.join(''))
+                $('#search-box').val('')
             },
             error: function(xhr, status, error) {
                 console.error(error);
