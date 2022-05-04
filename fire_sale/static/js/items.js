591751bb755c3c042426afb9c72d6b86ecdab7dc
anhthuvu512 <thuvu0512@gmail.com>
2022-05-04 12:39:37 +0000

    add search item

---
+++ fire_sale/static/js/items.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('#search-btn').on(types: 'click', selector: function (e) {
+        e.preventDefault();
+        var searchText = $('#search-box').val();
+        $.ajax(url: {
+            url: '/items?search_filter=' + searchText,
+            type: 'GET',
+            success: function(resp) {
+                var newHtml = resp.data.map(d =>{
+                    return 
+                })
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        })
+    });
+});
