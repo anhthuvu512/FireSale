18d9364c2d30eb33a94524cd61a24e84e0f5baee
anhthuvu512 <thuvu0512@gmail.com>
2022-05-05 11:33:51 +0000

    Update data.sql

--- fire_sale/data.sql
+++ fire_sale/data.sql
@@ -29,10 +29,27 @@ insert into firesale_item(name, highest_offer, condition, description, available
 insert into firesale_item(name, highest_offer, condition, description, available, seller_id) values ('T-shirt', 0, 'Good', 'A cute cat t-shirt', true, 1);
 insert into firesale_item(name, highest_offer, condition, description, available, seller_id) values ('Old camera', 0, 'Good', 'Useful camera', true, 2);
 
-delete from firesale_itemimage;
-
 insert into firesale_itemimage(image, item_id)
 Values('https://images.unsplash.com/photo-1625229086762-f06307638717?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',1),
       ('https://images.unsplash.com/photo-1615486261304-6978e38f377a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',2),
       ('https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',3),
       ('https://images.unsplash.com/photo-1586527484765-979a20639316?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',4);
+
+insert into firesale_offer(price, accepted, buyer_id, item_id) values (1599, false, 1, 1)
+
+create function CheckOfferPrice() returns trigger
+as $$ begin
+    if (new.price > (select highest_offer
+                     from firesale_item
+                     where id = new.item_id)) then
+        update firesale_item
+        set highest_offer = new.price
+        where id = new.item_id;
+    end if;
+    return new;
+end; $$ language  plpgsql;
+
+create trigger CheckOfferPrice
+after insert on firesale_offer
+for each row execute procedure CheckOfferPrice();
+
