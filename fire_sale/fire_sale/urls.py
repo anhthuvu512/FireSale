9b567833c916f8ec96218915fd7fb6331cc107e2
anhthuvu512 <thuvu0512@gmail.com>
2022-05-11 13:06:05 +0000

    base html for sign in and sign up

--- fire_sale/fire_sale/urls.py
+++ fire_sale/fire_sale/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('firesale.urls')),
+    path('', include('user.urls')),
     path('admin/', admin.site.urls),
     path('sales/', include('firesale.urls')),
     path('user/', include('user.urls'))
